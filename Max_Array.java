022916171db8b2c3bffb47a894923010b9f7f047
Prachi Thakkar <thakkarp710@gmail.com>
2026-03-04 21:02:15 +0530

    Day 1 – Array practice programs

---
+++ Max_Array.java
@@ -0,0 +1,51 @@
+//import java.util.Scanner;
+//public class Max_Array{
+//    public static void main(String[] args) {
+//        System.out.println("Finding max array");
+//Scanner sc = new Scanner(System.in);
+//System.out.println("Enter the list");
+//int n = sc.nextInt();
+//int arr[] = new int[n];
+//System.out.println("Enter the elements");
+//for (int i=0; i<n; i++) {
+//    arr[i]=sc.nextInt();
+//}
+//int max = arr[0];
+//for (int i=1;i<n;i++) {
+//    if (arr[i]>max) {
+//        max = arr[i];
+//    }
+//    }
+//        System.out.println("Maximum element is: " + max);
+//        }
+//    }
+import java.util.Scanner;
+
+public class Max_Array {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of elements: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        System.out.println("Enter the elements:");
+
+        for(int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int max = arr[0];
+
+        for(int i = 1; i < n; i++) {
+            if(arr[i] > max) {
+                max = arr[i];
+            }
+        }
+
+        System.out.println("Maximum element is: " + max);
+    }
+}
