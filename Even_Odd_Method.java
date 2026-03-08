45d5f0373dac7bfa7071be04e783cc8d00041600
Prachi Thakkar <thakkarp710@gmail.com>
2026-03-08 13:04:27 +0530

    Add files via upload
    This program checks whether a given number is even or odd using a separate method in Java. The user inputs a number, and the program calls a method that determines whether the number is divisible by 2. If the remainder is zero, the number is even; otherwise, it is odd. This implementation demonstrates the use of methods in Java to separate logic from the main program, making the code more organized, reusable, and easier to understand.

---
+++ Even_Odd_Method.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class Even_Odd_Method {
+    static int evenodd (int a){
+        if(a%2==0){
+System.out.println("even");
+        }
+        else {
+            System.out.println("odd");
+        }
+        return a;
+    }
+    public static void main(String[] args) {
+
+
+        System.out.println("Enter the number");
+
+        Scanner sc = new Scanner(System.in);
+        int i = sc.nextInt();
+        int c = evenodd(i);
+    }
+//        if (i % 2 == 0) {
+//            System.out.println("The number entered is even");
+//        } else {
+//            System.out.println("The number entered is odd");
+//
+//        }
+//    }
+}
